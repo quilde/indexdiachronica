fe9630084a9c9fc6fd9dfce3d06945a492fe1980
quilde <enseli.nils@gmail.com>
2024-01-27 18:11:59 +0100

    regex pipeline in rust

--- INDEX/explanation.md
+++ INDEX/explanation.md
@@ -1,3 +1,5 @@
+(this file is outdated.)
+
 {
     "pa": "North Omotic",
     "ch": "Wolaytta",
@@ -27,8 +29,6 @@
 }
 
 {
-    "parent": "North Omotic",
-    "child": "Wolaytta",
     "before": "b",
     "after": "w",
     "environment": "V_V",
@@ -38,14 +38,21 @@
     "original_contributor": "Mecislau",
     "reliability": "number",
     "notes": "",
-    "type": "Lenition"
-    "subtype": "Final Devoicing"
-    "name": "l-Deletion"
+    "type": "Lenition",
+    "subtype": "Final Devoicing",
+    "name": "l-Deletion",
+    "link": ""
 }
+
+https://chridd.nfshost.com/diachronica/
 
 https://en.wikipedia.org/wiki/Phonological_rule
 
 https://regexr.com/
+
+
+
+
 
 () (.*) to (\w*)\n*(.*)
 
@@ -55,3 +62,44 @@ https://regexr.com/
 (.*) → ([^/\n]*)\/?(.*)
 
 {\n    "before": "$1",\n    "after": "$2",\n    "env": "$3",\n    "src": [\n        "0"\n    ],\n    "or_auth": "Mecislau",\n    "rel": "unchecked",\n    "tag": [\n        ""\n    ],\n    "note": [\n        ""\n    ],\n\n}\n
+
+
+
+
+
+new: 
+
+([+-]) 
+
+$1
+
+(\S*) (\S*) ?(\S*)? → ([^/\n ]*) ([^/\n ]*)? ?([^/\n ]*)?\/?(.*)
+
+$1 → $4\n\n$2 → $5\n\n$3 → $6
+
+
+ → \n\n
+ 
+Replace
+
+
+(.*) → ([^/\n]*)\/?(.*)
+
+{\n    "before": "$1",\n    "after": "$2",\n    "env": "$3",\n    "rel": "unchecked",\n    "tag": [""],\n    "note": [\n        ""\n    ],\n    "type": "",\n    "subtype": "",\n    "name": "",\n    "link": [""]\n},\n
+
+,
+
+        {
+            "metadata": {
+                "parent": "North Omotic", 
+                "child": "North Omotic",
+                "or_contr": "Mecislau",
+                "source": "0",
+                "note": [
+                    ""
+                ]
+            },
+            "changeset": [
+                
+            ]
+        }
---
+++ diachronica_regex/src/main.rs
@@ -0,0 +1,150 @@
+use std::fs::File;
+use std::io::BufReader;
+use std::io::Read;
+use std::io::Write;
+
+use regex::Regex;
+
+use serde::{Deserialize, Serialize};
+
+use std::fs::write;
+
+#[derive(Debug, Serialize, Deserialize,)]
+struct Rule {
+    before: Vec<String>,
+    after: Vec<String>,
+    env: String,
+    notes: Vec<String>,
+    status: String,
+    tags: Vec<String>,
+    name_type: Vec<String>,
+}
+#[derive(Debug, Serialize, Deserialize,)]
+struct ChangeSet {
+    from: String,
+    to: String,
+    or_contr: String,
+    src: String,
+    changes: Vec<Rule>,
+}
+#[derive(Debug, Serialize, Deserialize,)]
+struct FamilySet {
+    family: String,
+    change_sets: Vec<ChangeSet>,
+}
+
+fn main() {
+    let mut s = read_file();
+    println!("{s}");
+    
+    let plusminus = Regex::new(r"(?<plusminus>[+-]) ").unwrap();
+    let s = plusminus.replace_all(s.as_str(), "$1").to_string();
+    let dashes = Regex::new(r"— ").unwrap();
+    let s = dashes.replace_all(s.as_str(), "").to_string();
+
+    let headings = Regex::new(r"([0-9]*\.)*[0-9] (?<fromto>(?<from>.*) to (?<to>[^\r\n]*))\r").unwrap();
+
+    let change_sets = Vec::<ChangeSet>::new();
+
+    let s = headings.replace_all(s.as_str(), "°$fromto").to_string();
+    println!("{s}");
+
+    let notes = Regex::new(r"(\r\n\r\n)(?<note>[^→\r\n]+)\r\n").unwrap();
+
+    let re =
+        Regex::new(r"(?<before>.*) → (?<after>[^/\r\n]*) ?\/? ?(?<env>[^/\r\n;]*)?;?(?<notes>[^\r\n]*)?")
+            .unwrap();
+
+    let change_sets_regex = Regex::new(r"(°)(?<from>.*) to (?<to>.*)\n\r\n(?<or_contr>[^,]*),(?<src>[^/\r\n]*)(?<changes>[^°]*)").unwrap();
+    
+    let parens = Regex::new(r"(°)(?<from>.*) to (?<to>.*)(?<changes>[^°]*)").unwrap();
+    
+    let change_sets: Vec<_> = change_sets_regex
+        .captures_iter(s.as_str())
+        .map(|c| {
+            let from = c.name("from").unwrap().as_str().to_string();
+            let to = c.name("to").unwrap().as_str().to_string();
+            let or_contr = c.name("or_contr").unwrap().as_str().to_string();
+            let src = c.name("src").unwrap().as_str().to_string();
+            
+            let changes2 = c.name("changes").unwrap().as_str().to_string();
+            println!("{changes2}");
+            let changes2 = notes.replace_all(changes2.as_str(), "$1 →  /;$note").to_string();
+
+            let mut output: Vec<&str> = Vec::new();
+
+            let changes: Vec<_> = re
+                .captures_iter(changes2.as_str())
+                .map(|c| {
+                    let before: Vec<String> = c.name("before").unwrap().as_str().to_string().split_terminator(" ").map(|c| {c.to_string()}).collect();
+                    let mut after: Vec<String> = c.name("after").unwrap().as_str().to_string().split_terminator(" ").map(|c| {c.to_string()}).collect();
+                    let env = c.name("env").unwrap().as_str().to_string();
+                    let note = c.name("notes").unwrap().as_str().to_string();
+                    
+                    let mut notes: Vec<String> = Vec::new();
+                    if note != "".to_string() {
+                        notes.push(note);
+                    } else {
+                        
+                    }
+                    
+                    while after.len() > before.len() {
+                        notes.push(after.pop().unwrap());
+                   }
+                   
+                   let status = "unchecked".to_string();
+                   let tags = vec!["generated".to_string()];
+                   let name_type = Vec::new();                   
+
+                    Rule { before, after, env, notes, status, tags, name_type }
+                })
+                .collect();
+
+            ChangeSet { from, to, or_contr, src,changes }
+        })
+        .collect();
+    
+    let family_set = FamilySet {
+        family: "TODO".to_string(),
+        change_sets,
+    };
+    /*
+    let change_sets_output = String::new();
+    
+    for set in change_sets {
+        let mut rules = String::new();
+        for rule in set.changes {
+            let output = json!({
+            "before": format!("{}", rule.before[0])
+            });
+            rules.push_str(serde_json::to_string(&output).unwrap().as_str());
+        }
+        let output = json!({
+        "family": "TODO",
+        "changesets": format!("{}", rules)
+        });
+    } */
+    let final_string = serde_json::to_string_pretty(&family_set).unwrap();
+    println!("{}", final_string);
+    
+    finish(final_string).expect("couldn't save file");
+    //dbg!(change_sets);
+}
+
+fn read_file() -> String {
+    let file =
+        File::open(r"C:/Users\ensel\Documents\nils\Programming\indexdiachronica\INDEX\input.txt")
+            .unwrap();
+    let mut buf_reader = BufReader::new(file);
+    let mut contents = String::new();
+    buf_reader.read_to_string(&mut contents).unwrap();
+    contents
+    // return contents;
+    // , std::IO::Error
+}
+
+fn finish(f: String)  -> std::io::Result<()> {
+    std::fs::write::<&str, &[u8]>(r"C:/Users\ensel\Documents\nils\Programming\indexdiachronica\INDEX\ouput.txt", f.as_ref())?;
+
+    Ok(())
+}
